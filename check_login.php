45d004a517557cfe7420f907d0aaa9fb1b69e1aa
TomWoo <tomw.82008@gmail.com>
2015-11-07 20:28:48 -0500

    check_login and sign_up

--- check_login.php
+++ check_login.php
@@ -1,15 +1,26 @@
 <?php
 
 //This gets all the other information from the form
-$item = $_POST['item'];
-$weight = $_POST['weight'];
-$cost =  $_POST['weight'];
+$name = $_POST['name'];
+$id = intval($_POST['id']);
 
 // Connects to your Database
 mysql_connect("localhost", "root", "bitnami") or die(mysql_error());
 mysql_select_db("my_database") or die(mysql_error());
 
-//Writes the information to the database
-mysql_query("INSERT INTO items_list VALUES ('$item', '$weight', '$cost')");
+$names_list = mysql_query("SELECT * FROM names_list") or die(mysql_error());
+while($row=mysql_fetch_array($names_list)) {
+    $name_row = $row['name'];
+    $id_row = $row['id'];
+    if(strcmp($name,$name_row)!=0 and $id==$id_row) {
+        $_SESSION['id'] = $id;
+        $_SESSION['name'] = $name;
+        $_SESSION['logged_in'] = 1;
+        session_start();
+        header("Location:http://colab-sbx-221.oit.duke.edu/index.html");
+        exit();
+    }
+}
+echo "Incorrect login info."
 
 ?>
